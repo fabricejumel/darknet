1a45d15a67bde4d159d25a75765871db5338accc
Fabrice Jumel <fabrice.jumel@gmail.com>
2020-12-17 22:47:35 +0100

    correct darknet use of .data arguments in detect mode causing segmentation fault

--- src/darknet.c
+++ src/darknet.c
@@ -475,8 +475,9 @@ int main(int argc, char **argv)
     } else if (0 == strcmp(argv[1], "detect")){
         float thresh = find_float_arg(argc, argv, "-thresh", .24);
 		int ext_output = find_arg(argc, argv, "-ext_output");
-        char *filename = (argc > 4) ? argv[4]: 0;
-        test_detector("cfg/coco.data", argv[2], argv[3], filename, thresh, 0.5, 0, ext_output, 0, NULL);
+        char *filename = (argc > 5) ? argv[5]: 0;
+        //FJ test_detector("cfg/coco.data", argv[2], argv[3], filename, thresh, 0.5, 0, ext_output, 0, NULL);
+	test_detector(argv[2], argv[3], argv[4], filename, thresh, 0.5, 0, ext_output, 0, NULL);
     } else if (0 == strcmp(argv[1], "cifar")){
         run_cifar(argc, argv);
     } else if (0 == strcmp(argv[1], "go")){
